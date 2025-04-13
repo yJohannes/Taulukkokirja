abd03f8333b4290bcd00b693f1ad2617794c249b
Johannes <141926330+yJohannes@users.noreply.github.com>
2025-04-13 20:26:50 +0300

    Mobile test

--- app.js
+++ app.js
@@ -89,6 +89,6 @@ app.get('/api/pages-structure', (req, res) => {
 });
 
 // Start the server and listen for incoming requests
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
