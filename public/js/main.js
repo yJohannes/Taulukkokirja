de89c8357d95ab236aded12f4ac2340bc367b3a2
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-04-15 16:03:49 +0300

    Add tooltips

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,35 @@
-import { initRipple } from './ripple.js';
-import { initLatex } from './latex.js';
-import { initTableHighlights } from './tables.js';
 import { loadExplorerToElement } from './explorer.js';
-import { loadPageToElement } from './pages.js';
-import { showPopup } from './popup.js';
 
 
 loadExplorerToElement('explorer-tabs');
 
-// Sidebar toggle functionality
+// Navbar
+let elements = [];
+
+elements.push(document.getElementById('latex-editor'));
+elements.push(document.getElementById('bookmarks'));
+elements.push(document.getElementById('settings'));
+elements.push(document.getElementById('sidebar-toggle'));
+
+console.log(elements)
+
+const params = {
+    delay: { show: 500, hide: 200 },
+    animation: true,
+    trigger: 'hover',  // No persisting tooltips
+}
+
+elements.forEach(element => {
+    element.setAttribute('data-toggle', 'tooltip');
+    element.setAttribute('data-placement', 'bottom');
+    $(element).tooltip(params);
+});
+
+
+// Sidebar collapse on mobile
 const sidebar = document.getElementById('sidebar');
 const toggleSidebar = document.getElementById('sidebar-toggle');
 
 toggleSidebar.addEventListener('click', () => {
     sidebar.classList.toggle('show');
 });
-
