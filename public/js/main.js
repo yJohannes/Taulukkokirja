37b8c3aa351415fed50014643f3e3b397ce4e396
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-04-16 19:23:16 +0300

    Clean up

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,14 @@
 import { loadExplorerToElement } from './explorer.js';
+import { initPageLoading } from './pages.js';
+import { initSidebar } from './sidebar.js';
+import { initNavbar } from './navbar.js';
+import { initRipple } from './ripple.js';
 
+document.addEventListener('DOMContentLoaded', async () => {
+    initPageLoading();
+    initNavbar();
+    initSidebar();
 
-loadExplorerToElement('explorer-tabs');
-
-// Navbar
-let elements = [];
-
-elements.push(document.getElementById('latex-editor'));
-elements.push(document.getElementById('bookmarks'));
-elements.push(document.getElementById('settings'));
-elements.push(document.getElementById('sidebar-toggle'));
-
-const params = {
-    delay: { show: 500, hide: 200 },
-    animation: true,
-    trigger: 'hover',  // No persisting tooltips
-}
-
-elements.forEach(element => {
-    element.setAttribute('data-toggle', 'tooltip');
-    element.setAttribute('data-placement', 'bottom');
-    $(element).tooltip(params);
-});
-
-
-// Sidebar collapse on mobile
-const sidebar = document.getElementById('sidebar');
-const sidebarToggle = document.getElementById('sidebar-toggle');
-
-sidebarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
+    await loadExplorerToElement(document.getElementById('explorer-container'));
+    await initRipple();
 });
