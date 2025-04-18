4d34b687e43960265a160e56d9dd222b6f551b51
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-04-18 23:19:13 +0300

    Clean up

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 const express = require('express');
 const compression = require('compression');
-
-// const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet');
 const path = require('path');
 const fs = require('fs');
 
-
-// const RATE_LIMIT = rateLimit({
-  //   windowMs: 15 * 60 * 1000,  // 15 minutes
-  //   max: 1000,                 // limit per IP
-  // });
-  
-  // app.use(RATE_LIMIT);
-  // app.use(
-    //   helmet.contentSecurityPolicy({
-      //     directives: {
-        //       defaultSrc: ["'self'"],
-        //       scriptSrc: ["'self'", "https://cdn.jsdelivr.net"],
-        //     },
-        //   })
-        // );
 const PORT = 5500;
 const app = express();
 
 app.use(compression());
-
-// Serve static files (HTML, CSS, JS) from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/pages-structure', (req, res) => {
@@ -46,7 +26,7 @@ app.get('/api/pages-structure', (req, res) => {
         result[file.name] = getFolderStructure(fullPath);
       } else {
         // If it's a file, add to the current folder's list
-        result[file.name] = null; // Or you can store file metadata here
+        result[file.name] = null;
       }
     });
 
@@ -55,13 +35,13 @@ app.get('/api/pages-structure', (req, res) => {
 
   try {
     const pageStructure = getFolderStructure(pagesDir);
-    res.json(pageStructure); // Send the folder structure as JSON
+    res.json(pageStructure);
   } catch (err) {
     res.status(500).send('Error reading directory');
   }
 });
 
-// Start the server and listen for incoming requests
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
