799491464d4b5aa90cadffbb2a6c03237e59df89
Johannes <141926330+yJohannes@users.noreply.github.com>
2025-04-27 21:45:46 +0300

    Add splitter

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,4 +1,6 @@
 
+
+
 function showSidebar(boolShow)
 {
     const sidebar = document.getElementById('sidebar');
@@ -27,9 +29,33 @@ function initSidebar()
         }
     });
 
+    const resizer = document.getElementById("main-splitter");
+    const contentWrapper = document.getElementById("content-wrapper");
+
+    let isResizing = false;
+    
+    resizer.addEventListener("mousedown", (e) => {
+        isResizing = true;
+        document.addEventListener("mousemove", resize);
+        document.addEventListener("mouseup", () => isResizing = false);
+    });
+    
+    function resize(e) {
+        if (!isResizing) return;
+        const clickX = e.clientX;
+        // Prevent jitter with min
+        const diff = Math.min(0, clickX - resizer.getBoundingClientRect().left); 
+
+        const width = Math.min(Math.max(250, clickX + diff), 500)
+        contentWrapper.style.gridTemplateColumns = `${width}px 5px minmax(0, 1fr)`;
+    }
+
     if (localStorage.getItem('sidebar-visibility') === 'show') {
         sidebar.classList.add('show');
     }
+
+
+
 }
 
 export {
