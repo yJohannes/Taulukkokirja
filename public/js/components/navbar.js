9552c073acecf4da159a5d4af748a5581ca90d34
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-02 19:55:02 +0300

    Add sidebar2

--- public/js/components/navbar.js
+++ public/js/components/navbar.js
@@ -1,14 +1,23 @@
 import { addRippleToElement } from "../effects/ripple.js";
 import { addToolTip } from "./tooltip.js";
+import { loadPageToElement } from "./pages.js";
 
 function initNavbar()
 {
+    const sidebar2 = document.getElementById('sidebar2');
+    const bookmarks = document.getElementById('bookmarks');
+    
+    bookmarks.addEventListener('click', () => {
+        sidebar2.classList.toggle('show');
+    });
+    
+    loadPageToElement('Kirjanmerkit.html', 'sidebar2-content', false)
+    
     let navs = [];
     navs.push(document.getElementById('latex-editor'));
-    navs.push(document.getElementById('bookmarks'));
+    navs.push(bookmarks);
     navs.push(document.getElementById('settings'));
-    navs.push(document.getElementById('sidebar-toggle'));
-
+    navs.push(document.getElementById('sidebar1-toggle'));
     navs.forEach(nav => {
         addToolTip(nav, 'bottom')
         addRippleToElement(nav);
