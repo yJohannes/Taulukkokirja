f5cc71da63cb150e2e9f4f2d603fab5359c95e56
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-02 17:53:49 +0300

    Remove redundant code

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,11 @@ import { loadExplorerToElement, loadExplorerSave } from './components/explorer/e
 import { initPageLoading } from './components/pages.js';
 import { initSidebar } from './components/sidebar.js';
 import { initNavbar } from './components/navbar.js';
-import { initRipple } from './effects/ripple.js';
 import { loadSettings } from './components/settings.js';
 
-const ACTIVE = 'active';
-const SHOW = 'show';
-const ARROW_FLIPPED = 'flipped';
+// const ACTIVE = 'active';
+// const SHOW = 'show';
+// const ARROW_FLIPPED = 'flipped';
 
 document.addEventListener('DOMContentLoaded', async () => {
     initPageLoading();
@@ -16,8 +15,5 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     await loadExplorerToElement(document.getElementById('explorer-container'));
     loadExplorerSave();
-
-    initRipple();
-
     loadSettings();
 });
