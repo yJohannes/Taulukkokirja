5a960140194ea111261b15fd8f3598369cadcd3e
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-08 15:55:27 +0300

    Update index.js

--- public/js/components/explorer/index.js
+++ public/js/components/explorer/index.js
@@ -3,3 +3,26 @@ export * from './defs.js'
 export * from './explorer.js'
 export * from './search.js'
 export * from './tab.js'
+
+export function formatPathLabel(tabPath) {
+    tabPath = decodeURIComponent(tabPath);
+    const split = tabPath.split('/')
+    const last = split.length - 1;
+
+    if (tabPath.endsWith('.html')) {
+        let name = '<b>' + split[last] + '</b>';
+        if (last > 0) {
+            name = name + ' | <small><i>' + split[last-1] + '</i></small>';
+        }
+        name = name.replace('.html', '');
+        return name;
+    } else {
+        let name = split[last];
+        if (last > 0) {
+            name = name + ' | <small>' + split[last-1] + '</small>';
+        }
+        name = '<i>' + name + '</i>';
+
+        return name;
+    }
+}
