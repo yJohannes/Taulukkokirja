e21a85a9ab9a074fe182fdf924810cb5fdd699e1
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-08 17:43:58 +0300

    Follow naming conventions & tweaks

--- public/js/components/explorer/search.js
+++ public/js/components/explorer/search.js
@@ -1,17 +1,21 @@
 import { loadPageToElement } from '../pages.js';
-import { getTabByPath, loadExplorerStructure, openPath, showExplorer } from './explorer.js';
-import { createTab } from './tab.js';
 import { addRippleToElement } from '../../effects/ripple.js';
+import { updateBookmarks } from '../bookmarks/index.js';
+import * as explorer from './index.js'
 
 let explorerStructure = null;
 
+function clearSearch() {
+    document.querySelector("#explorer-search").value = '';
+}
+
 function showResults(bool) {
-    const searchContainer = document.getElementById('search-container');
+    const $searchContainer = document.getElementById('search-container');
 
     if (bool) {
-        searchContainer.style.display = 'inline-block';
+        $searchContainer.style.display = 'inline-block';
     } else {
-        searchContainer.style.display = 'none';
+        $searchContainer.style.display = 'none';
     }
 
 }
@@ -41,9 +45,9 @@ function searchInStructure(structure, searchString, path = '') {
     return matches;
 }
 
-function generateResultView(matches, resultContainer) {
-    resultContainer.innerHTML = '';
-    resultContainer.innerHTML += `
+function generateResultView(matches, $container) {
+    $container.innerHTML = '';
+    $container.innerHTML += `
         <div style="display: flex; justify-content: space-between; align-items: center; padding: 1rem;">
             <p style="margin: 0"><b>Haun tulokset</b></p>
             <div class="rounded-pill" style="display: inline-block; font-size: 75%; white-space: nowrap; background-color: var(--color-primary); color: var(--color-secondary); padding: 0.4rem 0.6rem;">
@@ -53,77 +57,72 @@ function generateResultView(matches, resultContainer) {
     `
 
     if (matches.length === 0) {
-        resultContainer.innerHTML += '<p style="padding-left: 1rem;">Ei tuloksia</p>'
+        $container.innerHTML += '<p style="padding-left: 1rem;">Ei tuloksia</p>'
         return;
     }
 
     for (const match of matches) {
-        const split = match.split('/')
-        const last = split.length - 1;
         
-        let tab;
+        let $tab;
         if (match.endsWith('.html')) {
-            let name = '<b>' + split[last] + '</b>';
-            if (last > 0) {
-                name = '<small><i>' + split[last-1] + '</i></small> / ' + name;
-            }
+            const name = explorer.formatPathLabel(match);
 
             $tab = explorer.createTab(name, 0, false, 'pages/' + match);
 
-            tab.addEventListener('click', (e) => {
+            $tab.addEventListener('click', (e) => {
 
                 if (e.button === 0) {
                     loadPageToElement('pages/' + match, 'page-container');
                 }
+
+                updateBookmarks();
             });
 
             // Right click
-            tab.addEventListener('contextmenu', (e) => {
+            $tab.addEventListener('contextmenu', (e) => {
                 e.preventDefault();
 
-                resultContainer.innerHTML = '';
-                showExplorer(true);
+                $container.innerHTML = '';
+                explorer.showExplorer(true);
                 showResults(false);
 
                 const parentPath = 'pages/' + match.substring(0, match.lastIndexOf('/'));
 
-                openPath(parentPath);
-                getTabByPath(parentPath).scrollIntoView({
+                explorer.openPath(parentPath);
+                explorer.getTabByPath(parentPath).scrollIntoView({
                     behavior: 'auto',     // smooth scrolling animation
                     block: 'center',        // align element to center of the viewport
-                  });
+                });
+
+                clearSearch();
             });
         } else {
-            let name = split[last];
-            if (last > 0) {
-                name = '<small>' + split[last-1] + '</small> / ' + name;
-            }
+            const name = explorer.formatPathLabel(match);
 
-            name = '<i>' + name + '</i>';
-
-            tab = createTab(name.replace('.html', ''), 1, false, resultContainer);
-            tab.addEventListener('click', () => {
-                resultContainer.innerHTML = '';
-                showExplorer(true);
+            $tab = explorer.createTab(name, 0, false, 'pages/' + match);
+            $tab.addEventListener('click', () => {
+                $container.innerHTML = '';
+                explorer.showExplorer(true);
                 showResults(false);
 
                 explorer.openPath('pages/' + match);
                 explorer.getTabByPath('pages/' + match).scrollIntoView({
                     behavior: 'auto',     // smooth scrolling animation
                     block: 'center',        // align element to center of the viewport
-                  });
+                });
+
+                clearSearch();
             });
         }
 
-        addRippleToElement(tab);
-        resultContainer.appendChild(tab);
+        addRippleToElement($tab);
+        $container.appendChild($tab);
     }
 }
 
 function search(searchString) {
-
     if (searchString.length <= 0) {
-        showExplorer(true);
+        explorer.showExplorer(true);
         showResults(false);
         
         document.getElementById('explorer-search-icon').style.display = 'inline';
@@ -132,7 +131,7 @@ function search(searchString) {
         return;
     }
 
-    showExplorer(false);
+    explorer.showExplorer(false);
     showResults(true);
 
     document.getElementById('explorer-search-icon').style.display = 'none';
@@ -148,34 +147,34 @@ function search(searchString) {
     generateResultView(matches, searchContainer)
 }
 
-async function initSearchToInput(element)
+async function initSearchToInput($element)
 {
     explorerStructure = (await explorer.loadExplorerStructure()).pages;
 
-    element.addEventListener('input', (event) => {
+    $element.addEventListener('input', (event) => {
         search(event.target.value);
     });
 
-    const clearBtn = document.getElementById('explorer-clear-search');
-    const searchInput = document.querySelector("#explorer-search");
+    const $clear = document.getElementById('explorer-clear-search');
+    const $search = document.querySelector("#explorer-search");
     
-    addRippleToElement(clearBtn);
+    addRippleToElement($clear);
 
-    clearBtn.addEventListener("click", () => {
-        searchInput.value = '';
+    $clear.addEventListener("click", () => {
+        clearSearch();
 
         // simulate input
         const event = new Event('input', { bubbles: true });
-        searchInput.dispatchEvent(event);
+        $search.dispatchEvent(event);
 
-        searchInput.focus();
+        $search.focus();
     });
 
     document.addEventListener("keydown", function(event) {
         if (event.altKey && event.key === "s") {
             event.preventDefault();
-            if (searchInput) {
-                searchInput.focus();
+            if ($search) {
+                $search.focus();
             }
         }
     });
