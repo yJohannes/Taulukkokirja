631be711c70836b54c0df8d4ed0a0c06f5bdfcfe
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-08 17:42:38 +0300

    Create index.js

---
+++ public/js/components/bookmarks/index.js
@@ -0,0 +1,74 @@
+import * as storage from '../storage/index.js';
+import { loadPageToElement,  } from '../pages.js';
+import { sanitizePath } from '../pages.js';
+import { addRippleToElement } from '../../effects/ripple.js';
+
+import * as explorer from '../explorer/index.js';
+
+
+
+function createTabFromList(pathsList, container, deleteTargetListName=null) {
+    container.innerHTML = '';
+
+    pathsList.forEach(path => {
+        const name = explorer.formatPathLabel(path);
+
+        const tab = explorer.createTab(name, 0, false, path);
+        tab.style.padding = '8px';
+        addRippleToElement(tab);
+
+        /* 
+            <button id="explorer-clear-search" class="btn rounded-circle ripple ripple-dark ripple-centered hover-glow" style="display: none;">
+                <i class="bi bi-x-lg" style="font-size: 120%;"></i>
+            </button>
+        */
+       if (deleteTargetListName) {
+            const delBtn = document.createElement('button');
+            delBtn.classList.add('btn', 'rounded-circle', 'ripple', 'ripple-dark', 'ripple-centered', 'hover-glow')
+            delBtn.classList.add('button-with-icon');
+            addRippleToElement(delBtn);
+
+            delBtn.addEventListener('click', (e) => {
+                e.stopPropagation(); // Prevents event from reaching <a>
+                e.preventDefault();
+
+                storage.removeFromStorageList(deleteTargetListName, path);
+                updateBookmarks();
+            });
+
+            const x = document.createElement('i');
+            x.classList.add('bi', 'bi-trash3-fill');
+            x.style.fontSize = '120%'
+
+            delBtn.appendChild(x);
+            tab.appendChild(delBtn);
+        }
+
+        tab.addEventListener('click', (e) => {
+            loadPageToElement(path, 'page-container');
+
+            console.log(path)
+            explorer.openPath(path);
+            tab.scrollIntoView({
+                behavior: 'auto',
+                block: 'center'
+            });
+
+            tab.addEventListener('click', () => updateBookmarks());
+        });
+
+        container.appendChild(tab);
+    });
+}
+
+export function updateBookmarks() {
+    const bookmarksContainer = document.getElementById('bookmark-container');
+    const recentsContainer = document.getElementById('recently-viewed-container');
+
+    const bookmarks = storage.getFromStorageList('bookmarks');
+    const recents = storage.getFromStorageList('recently-viewed');
+    
+    // Use the function for both lists
+    createTabFromList(bookmarks, bookmarksContainer, 'bookmarks');
+    createTabFromList(recents, recentsContainer);
+}
