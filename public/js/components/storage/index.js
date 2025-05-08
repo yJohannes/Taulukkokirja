3483911cb7c87f122151a5d9645b859231119b80
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-08 15:45:30 +0300

    Improve naming

--- public/js/components/storage/index.js
+++ public/js/components/storage/index.js
@@ -1,22 +1,32 @@
-export function getFromStorageList(key) {
-    const raw = localStorage.getItem(key) || '[]';
+export function getFromStorageList(listName) {
+    const raw = localStorage.getItem(listName) || '[]';
     return JSON.parse(raw);
 }
 
-export function addToStorageList(key, item) {
-    let list = getFromStorageList(key);
+export function addToStorageList(listName, item, prepend=false) {
+    let list = getFromStorageList(listName);
+
 
     if (!list.includes(item)) {
-        list.push(item);
-        localStorage.setItem(key, JSON.stringify(list));
+        if (prepend) {
+            list.unshift(item);
+        } else {
+            list.push(item);
+        }
+        localStorage.setItem(listName, JSON.stringify(list));
     }
 }
-export function removeFromStorageList(key, item) {
-    let list = getFromStorageList(key);
+
+export function removeFromStorageList(listName, item) {
+    let list = getFromStorageList(listName);
 
     const index = list.indexOf(item);
     if (index !== -1) {
         list.splice(index, 1);
-        localStorage.setItem(key, JSON.stringify(list));
+        localStorage.setItem(listName, JSON.stringify(list));
     }
+}
+
+export function setStorageItem(itemName, item) {
+    localStorage.setItem(itemName, JSON.stringify(item));
 }
