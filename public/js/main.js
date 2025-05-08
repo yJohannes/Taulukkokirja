f452c6383ef7514323e245f2f633bd18664ae8bb
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-08 17:38:54 +0300

    rm

--- public/js/main.js
+++ public/js/main.js
@@ -19,19 +19,3 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     editor.init();
 
-    const baseOpts = {
-        baseUrl: 'http://localhost:5500',
-        initialValue:
-            'testi. <br>kaava: <img src="http://localhost:5500/math.svg?latex=%5Csqrt%7B123%7D" alt="\\sqrt{123}"><br> <br>',
-        textAreaProps: {
-            ariaLabelledBy: 'question-label',
-            questionId: '123',
-            editorStyle: {},
-            lang: 'fi-FI',
-        },
-        // onValueChange: console.log
-    }
-
-    window.makeRichText(baseOpts)
-
-});
