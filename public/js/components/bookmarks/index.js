6b00c5a6d7379fb6a0ff58102c512cc56d00082b
Johannes <141926330+yJohannes@users.noreply.github.com>
2025-05-09 22:14:34 +0300

    Update index.js

--- public/js/components/bookmarks/index.js
+++ public/js/components/bookmarks/index.js
@@ -7,21 +7,18 @@ import * as explorer from '../explorer/index.js';
 
 
 
-function createTabFromList(pathsList, $container, deleteTargetListName=null) {
+function createTabsFromList(pathsList, $container, deleteTargetListName=null) {
     $container.innerHTML = '';
 
     pathsList.forEach(path => {
         const name = explorer.formatPathLabel(path);
 
         const $tab = explorer.createTab(name, 0, false, path);
-        $tab.style.padding = '8px';
+        $tab.style.setProperty('padding', '0.5rem', 'important');
+        $tab.style.setProperty('padding-left', '0.75rem', 'important');
+
         addRippleToElement($tab);
 
-        /* 
-            <button id="explorer-clear-search" class="btn rounded-circle ripple ripple-dark ripple-centered hover-glow" style="display: none;">
-                <i class="bi bi-x-lg" style="font-size: 120%;"></i>
-            </button>
-        */
        if (deleteTargetListName) {
             const $delete = document.createElement('button');
             $delete.classList.add('btn', 'rounded-circle', 'ripple', 'ripple-dark', 'ripple-centered', 'hover-glow')
@@ -31,14 +28,13 @@ function createTabFromList(pathsList, $container, deleteTargetListName=null) {
             $delete.addEventListener('click', (e) => {
                 e.stopPropagation(); // Prevents event from reaching <a>
                 e.preventDefault();
-
                 storage.removeFromStorageList(deleteTargetListName, path);
                 updateBookmarks();
             });
 
             const $x = document.createElement('i');
             $x.classList.add('bi', 'bi-trash3-fill');
-            $x.style.fontSize = '120%'
+            $x.style.fontSize = '110%'
 
             $delete.appendChild($x);
             $tab.appendChild($delete);
@@ -67,7 +63,7 @@ export function updateBookmarks() {
     const bookmarks = storage.getFromStorageList('bookmarks');
     const recents = storage.getFromStorageList('recently-viewed');
     
-    // Use the function for both lists
-    createTabFromList(bookmarks, $bookmarksContainer, 'bookmarks');
-    createTabFromList(recents, $recentsContainer);
+    
+    createTabsFromList(bookmarks, $bookmarksContainer, 'bookmarks');
+    createTabsFromList(recents, $recentsContainer);
 }
