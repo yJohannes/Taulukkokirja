a686afea3331c843fd1d544927e19f738d518972
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-13 23:07:43 +0300

    Fix 100vh bug on mobile

---
+++ public/js/layout/grid.js
@@ -0,0 +1,4 @@
+export function setRealVH() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,16 @@
-import * as explorer from './components/explorer/index.js'
-
+import { setRealVH } from './layout/grid.js';
 import { initSplitGrid } from './components/split-grid.js';
 import { initPageLoading } from './pages/index.js';
 import { initSidebar } from './layout/sidebar.js';
 import { initNavbar } from './layout/navbar.js';
 import { loadSettings } from './components/settings.js';
+import * as explorer from './components/explorer/index.js'
 import * as editor from './rich-text-editor/index.js'
 import * as bookmarks from './components/bookmarks/index.js';
 import * as search from './components/search/index.js';
+
+window.addEventListener('resize', setFullHeight);
+window.addEventListener('load', setFullHeight);
 
 document.addEventListener('DOMContentLoaded', async () => {
     initSplitGrid();
