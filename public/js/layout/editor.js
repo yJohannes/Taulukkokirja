d091da00a626cfcab23eef07f0d4fff6981a5872
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-05-15 21:09:19 +0300

    Fix editor state in grid not saving

--- public/js/layout/editor.js
+++ public/js/layout/editor.js
@@ -1,28 +1,40 @@
-const grid = document.getElementById('content-wrapper')
-const editor = document.getElementById('editor')
+import { saveGridState } from "../components/split-grid.js";
 
-let hidden
-let cachedPageHeight
-let cachedEditorHeight
+const grid = document.getElementById('content-wrapper');
+const editor = document.getElementById('editor');
+
+let cachedPageHeight = '0px';
+let cachedEditorHeight = '0px';
 
 export function toggleEditor() {
-    hidden ? showEditor() : hideEditor();
+    const rowStyle = grid.style.gridTemplateRows;
+    const split = rowStyle.split(' ');
+
+    const isHidden = split[split.length - 1] === '0px';
+    isHidden ? showEditor() : hideEditor();
 }
 
 export function showEditor() {
-    const rowStyle = grid.style.gridTemplateRows
-    const split = rowStyle.split(' ')
-    grid.style.gridTemplateRows = `${cachedPageHeight} 1px ${cachedEditorHeight}`
-    hidden = false 
+    const rowStyle = grid.style.gridTemplateRows;
+    const split = rowStyle.split(' ');
+    
+    if (cachedEditorHeight === '0px')
+        grid.style.gridTemplateRows = `2fr 1px 1fr`;
+    else
+        grid.style.gridTemplateRows = `${cachedPageHeight} 1px ${cachedEditorHeight}`;
+
+    saveGridState();
 }
 
 export function hideEditor() {
-    const rowStyle = grid.style.gridTemplateRows
-    const split = rowStyle.split(' ')
-    cachedPageHeight = split[0]
-    cachedEditorHeight = split[2]
-    grid.style.gridTemplateRows = `1fr 0px 0px`
-    hidden = true
+    const rowStyle = grid.style.gridTemplateRows;
+    const split = rowStyle.split(' ');
+    
+    cachedPageHeight = split[0];
+    cachedEditorHeight = split[2];
+
+    grid.style.gridTemplateRows = `1fr 0px 0px`;
+    saveGridState();
 }
 
 /*
@@ -31,33 +43,33 @@ rowMinSizes:
     2: 44,
 */
 export function maximizeEditor() {
-    const gridHeight = parseFloat(getComputedStyle(grid).height)
-    const row0Ratio = 300 / gridHeight
-    const row2Ratio = 1 - row0Ratio
+    const gridHeight = parseFloat(getComputedStyle(grid).height);
+    const row0Ratio = 300 / gridHeight;
+    const row2Ratio = 1 - row0Ratio;
 
-    grid.style.gridTemplateRows = `${row0Ratio}fr 1px ${row2Ratio}fr`
+    grid.style.gridTemplateRows = `${row0Ratio}fr 1px ${row2Ratio}fr`;
 }
 export function minimizeEditor() {
-    const gridHeight = parseFloat(getComputedStyle(grid).height)
-    const row2Ratio = 44 / gridHeight
-    const row0Ratio = 1 - row2Ratio
+    const gridHeight = parseFloat(getComputedStyle(grid).height);
+    const row2Ratio = 44 / gridHeight;
+    const row0Ratio = 1 - row2Ratio;
 
-    grid.style.gridTemplateRows = `${row0Ratio}fr 1px ${row2Ratio}fr`
+    grid.style.gridTemplateRows = `${row0Ratio}fr 1px ${row2Ratio}fr`;
 }
 
 
-const maximizeBtn = document.getElementById('maximize-editor')
-const minimizeBtn = document.getElementById('minimize-editor')
-const closeBtn = document.getElementById('close-editor')
+const maximizeBtn = document.getElementById('maximize-editor');
+const minimizeBtn = document.getElementById('minimize-editor');
+const closeBtn = document.getElementById('close-editor');
 
 minimizeBtn.addEventListener('click', () => {
-    minimizeEditor()
+    minimizeEditor();
 })
 
 maximizeBtn.addEventListener('click', () => {
-    maximizeEditor()
+    maximizeEditor();
 })
 
 closeBtn.addEventListener('click', () => {
-    hideEditor()
+    hideEditor();
 })
