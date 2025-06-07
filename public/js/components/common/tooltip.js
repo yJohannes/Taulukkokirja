5430711788ddd0affc066f3d3eaec1cf0b7b8071
Johannes <141926330+yJohannes@users.noreply.github.com>
2025-06-07 16:34:14 +0300

    Upgrade to Bootstrap 5

--- public/js/components/common/tooltip.js
+++ public/js/components/common/tooltip.js
@@ -1,14 +1,18 @@
+const toolTipOptions = {
+    delay: { show: 500, hide: 200 },
+    animation: true,
+    trigger: 'hover'  // No persisting tooltips
+};
+
 export function addToolTip($element, placement, tip=null) {
-    $element.setAttribute('data-toggle', 'tooltip');
-    $element.setAttribute('data-placement', placement);
+    $element.setAttribute('data-bs-toggle', 'tooltip');
+    $element.setAttribute('data-bs-placement', placement);
     
     if (tip) {
         $element.setAttribute('title', tip);
     }
-    
-    $($element).tooltip({
-        delay: { show: 500, hide: 200 },
-        animation: true,
-        trigger: 'hover'  // No persisting tooltips
-    });
+
+    new bootstrap.Tooltip($element, toolTipOptions);
+
+    // $($element).tooltip();
 }
