f30b39c59ec1b48356cf3aa948f46499e7b46f58
Johannes <141926330+yJohannes@users.noreply.github.com>
2025-06-12 18:38:59 +0300

    Update editor.js

--- public/js/layout/editor.js
+++ public/js/layout/editor.js
@@ -1,5 +1,6 @@
 import { saveGridState } from "../components/split-grid.js";
 
+const GUTTER_HEIGHT = '64px';
 const grid = document.getElementById('content-wrapper');
 
 let cachedPageHeight = '0px';
@@ -15,9 +16,9 @@ export function toggleEditor() {
 
 export function showEditor() {
     if (cachedEditorHeight === '0px')
-        grid.style.gridTemplateRows = `2fr 1px 1fr`;
+        grid.style.gridTemplateRows = `2fr ${GUTTER_HEIGHT} 1fr`;
     else
-        grid.style.gridTemplateRows = `${cachedPageHeight} 1px ${cachedEditorHeight}`;
+        grid.style.gridTemplateRows = `${cachedPageHeight} ${GUTTER_HEIGHT} ${cachedEditorHeight}`;
 
     saveGridState();
 }
@@ -38,14 +39,14 @@ export function maximizeEditor() {
     const row0Ratio = 300 / gridHeight;
     const row2Ratio = 1 - row0Ratio;
 
-    grid.style.gridTemplateRows = `${row0Ratio}fr 1px ${row2Ratio}fr`;
+    grid.style.gridTemplateRows = `${row0Ratio}fr ${GUTTER_HEIGHT} ${row2Ratio}fr`;
 }
 export function minimizeEditor() {
     const gridHeight = parseFloat(getComputedStyle(grid).height);
     const row2Ratio = 44 / gridHeight;
     const row0Ratio = 1 - row2Ratio;
 
-    grid.style.gridTemplateRows = `${row0Ratio}fr 1px ${row2Ratio}fr`;
+    grid.style.gridTemplateRows = `${row0Ratio}fr ${GUTTER_HEIGHT} ${row2Ratio}fr`;
 }
 
 
