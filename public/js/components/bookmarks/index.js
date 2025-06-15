7edf9574694b33d60c64f53aa8d5e5b857001233
Johannes <141926330+yJohannes@users.noreply.github.com>
2025-06-15 18:50:37 +0300

    Update index.js

--- public/js/components/bookmarks/index.js
+++ public/js/components/bookmarks/index.js
@@ -1,39 +1,41 @@
 import * as storage from '../storage/index.js';
 import * as explorer from '../explorer/index.js';
+import { formatting } from '../../pages/formatting.js';
 
-function createTabsFromList(pathsList, $container, deleteTargetListName=null) {
-    $container.innerHTML = '';
+
+function createTabsFromList(pathsList, container, deleteTargetListName=null) {
+    container.innerHTML = '';
 
     pathsList.forEach(path => {
         const name = formatting.formatPathToLabel(path);
 
-        const $tab = explorer.createTab(name, 0, false, path);
-        $tab.style.setProperty('padding', '0.5rem', 'important');
-        $tab.style.setProperty('padding-left', '0.75rem', 'important');
+        const tab = explorer.createTab(name, 0, false, path);
+        tab.style.setProperty('padding', '0.5rem', 'important');
+        tab.style.setProperty('padding-left', '0.75rem', 'important');
 
        if (deleteTargetListName) {
-            const $delete = document.createElement('button');
-            $delete.classList.add('btn', 'rounded-circle', 'ripple', 'ripple-dark', 'ripple-centered', 'hover-glow')
-            $delete.classList.add('icon-button');
-            $delete.style.fontSize = '1rem';
+            const deleteBtn = document.createElement('button');
+            deleteBtn.classList.add('btn', 'rounded-circle', 'ripple', 'ripple-dark', 'ripple-centered', 'hover-glow')
+            deleteBtn.classList.add('icon-button');
+            deleteBtn.style.fontSize = '1rem';
 
-            $delete.addEventListener('click', (e) => {
+            deleteBtn.addEventListener('click', (e) => {
                 e.stopPropagation(); // Prevents event from reaching <a>
                 e.preventDefault();
                 storage.removeFromStorageList(deleteTargetListName, path);
                 updateBookmarks();
             });
 
-            const $x = document.createElement('i');
-            $x.classList.add('bi', 'bi-trash3');
+            const x = document.createElement('i');
+            x.classList.add('bi', 'bi-trash3');
 
-            $delete.appendChild($x);
-            $tab.appendChild($delete);
+            deleteBtn.appendChild(x);
+            tab.appendChild(deleteBtn);
         }
 
-        $tab.addEventListener('click', (e) => {
+        tab.addEventListener('click', (e) => {
             explorer.openPath(path);
-            $tab.scrollIntoView({
+            tab.scrollIntoView({
                 behavior: 'auto',
                 block: 'center'
             });
@@ -41,19 +43,19 @@ function createTabsFromList(pathsList, $container, deleteTargetListName=null) {
             updateBookmarks();
         });
 
-        $container.appendChild($tab);
+        container.appendChild(tab);
     });
 }
 
 export function updateBookmarks() {
-    const $bookmarksContainer = document.getElementById('bookmark-container');
-    const $recentsContainer = document.getElementById('recently-viewed-container');
+    const bookmarksContainer = document.getElementById('bookmark-container');
+    const recentsContainer = document.getElementById('recently-viewed-container');
 
     const bookmarks = storage.getFromStorageList('bookmarks');
     const recents = storage.getFromStorageList('recently-viewed');
     
-    createTabsFromList(bookmarks, $bookmarksContainer, 'bookmarks');
-    createTabsFromList(recents, $recentsContainer);
+    createTabsFromList(bookmarks, bookmarksContainer, 'bookmarks');
+    createTabsFromList(recents, recentsContainer);
 }
 
 export function addBookmarkToHeader(header, {justify = 'space-between', align = 'center'} = {}) {
@@ -70,22 +72,22 @@ export function addBookmarkToHeader(header, {justify = 'space-between', align = 
 }
 
 export function createBookmarkButton(pagePath) {
-    const $button = document.createElement('button');
-    $button.classList.add('btn', 'icon-button', 'rounded-circle', 'ripple', 'ripple-dark', 'ripple-centered', 'hover-glow');
+    const button = document.createElement('button');
+    button.classList.add('btn', 'icon-button', 'rounded-circle', 'ripple', 'ripple-dark', 'ripple-centered', 'hover-glow');
     
-    const $headerWrapper = document.createElement('h1');
-    $headerWrapper.style.margin = '0';
-    $headerWrapper.style.color = 'inherit';
+    const headerWrapper = document.createElement('h1');
+    headerWrapper.style.margin = '0';
+    headerWrapper.style.color = 'inherit';
 
-    const $icon = document.createElement('i');
-    $headerWrapper.appendChild($icon);
-    $button.appendChild($headerWrapper);
-    $button.addEventListener('click', () => {
-        if ($icon.classList.toggle('bi-bookmark')) {
+    const icon = document.createElement('i');
+    headerWrapper.appendChild(icon);
+    button.appendChild(headerWrapper);
+    button.addEventListener('click', () => {
+        if (icon.classList.toggle('bi-bookmark')) {
             storage.removeFromStorageList('bookmarks', pagePath)
         }
         
-        if ($icon.classList.toggle('bi-bookmark-fill')) {
+        if (icon.classList.toggle('bi-bookmark-fill')) {
             storage.addToStorageList('bookmarks', pagePath, true)
         }
 
@@ -95,10 +97,10 @@ export function createBookmarkButton(pagePath) {
     const bookmarks = storage.getFromStorageList('bookmarks');
     
     if (bookmarks.includes(pagePath)) {
-        $icon.classList.add('bi', 'bi-bookmark-fill');
+        icon.classList.add('bi', 'bi-bookmark-fill');
     } else {
-        $icon.classList.add('bi', 'bi-bookmark');
+        icon.classList.add('bi', 'bi-bookmark');
     }
 
-    return $button;
+    return button;
 }
