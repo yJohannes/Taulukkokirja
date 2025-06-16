c64816244e40e1bd2eb21655ce43f49179fef885
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-06-16 19:08:11 +0300

    Rename

--- public/js/rich-text-editor/index.js
+++ public/js/rich-text-editor/index.js
@@ -1,1 +1,20 @@
-export * from "./init.js";
+export const editor = {
+    init,
+}
+
+function init() {
+    const baseOpts = {
+        baseUrl: 'http://localhost:5500',
+        initialValue: '',
+            // 'testi. <br>kaava: <img src="http://localhost:5500/math.svg?latex=%5Csqrt%7B123%7D" alt="\\sqrt{123}"><br> <br>',
+        textAreaProps: {
+            ariaLabelledBy: 'question-label',
+            questionId: '123',
+            editorStyle: {},
+            lang: 'fi-FI',
+        },
+        // onValueChange: console.log
+    }
+    
+    window.makeRichText(baseOpts)
+}
