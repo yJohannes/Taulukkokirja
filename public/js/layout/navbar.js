b123d6d34da56b6cce9d04424ee1d6672e62017d
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-06-20 00:01:59 +0300

    Better sidebar toggling on mobile

--- public/js/layout/navbar.js
+++ public/js/layout/navbar.js
@@ -4,6 +4,9 @@ import { updateBookmarks } from "../components/bookmarks/bookmarks.js";
 import { utils } from "../common/utils.js";
 import { Editor } from "../rich-text-editor/index.js";
 
+import { SplitGrid } from "./split-grid.js";
+import { toggleGridColumn } from "./workspace.js";
+
 export const Navbar = {
     init,
 }
@@ -11,11 +14,13 @@ export const Navbar = {
 function init() {
     const sidebar1 = document.getElementById('sidebar-1');
     const sidebar2 = document.getElementById('sidebar-2');
+    
     const sidebarToggle = document.getElementById('nav-sidebar-toggle');
-    const editor = document.getElementById('nav-latex-editor');
-    const geogebra = document.getElementById('nav-geogebra');
     const history = document.getElementById('nav-history');
     const bookmarks = document.getElementById('nav-bookmarks');
+    const editor = document.getElementById('nav-latex-editor');
+    // const rightSidebar = document.getElementById('nav-toggle-right-sidebar');
+    const geogebra = document.getElementById('nav-geogebra');
     const darkMode = document.getElementById('nav-dark-mode');
     const settings = document.getElementById('nav-settings');
 
@@ -24,12 +29,35 @@ function init() {
         updateBookmarks();
         sidebar2.classList.remove('show');
     });
-    bookmarks.addEventListener('click', () => {
+
+    const bookmarkRoot = document.getElementById('bookmarks');
+    const historyRoot = document.getElementById('recently-viewed');
+
+    function updateSidebarVisibility() {
+        if (utils.isElementVisible(historyRoot) || utils.isElementVisible(bookmarkRoot)) {
+            sidebar2.classList.add('show');
+            sidebar1.classList.remove('show');
+        } else {
+            sidebar2.classList.remove('show');
+        }
+    }
+
+    history.addEventListener('click', () => {
+        utils.toggleVisibility(historyRoot);
+        updateSidebarVisibility();
         updateBookmarks();
-        sidebar1.classList.remove('show');
-        sidebar2.classList.toggle('show');
+    });
+
+    bookmarks.addEventListener('click', () => {
+        utils.toggleVisibility(bookmarkRoot);
+        updateSidebarVisibility();
+        updateBookmarks();
     });
     
+    // rightSidebar.addEventListener('click', () => {
+    //     toggleGridColumn(SplitGrid.ref, 4, !utils.isElementVisible(sidebar2));
+    // });
+
     const geogebraRoot = document.getElementById('geogebra-iframe-root');
     const editorRoot = document.getElementById('rich-text-editor-root');
 
