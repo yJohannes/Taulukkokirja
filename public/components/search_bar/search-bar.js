2263dd219b862fbf6842a2751f1cbf9e36389b8d
Johannes <141926330+yJohannes@users.noreply.github.com>
2025-06-23 17:14:02 +0300

    Update px to rem & namespace classes

--- public/components/search_bar/search-bar.js
+++ public/components/search_bar/search-bar.js
@@ -30,11 +30,9 @@ function makeSearchBar(input) {
     // Create search icon SVG
     const searchIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     searchIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    searchIcon.setAttribute('width', '16');
-    searchIcon.setAttribute('height', '16');
+    searchIcon.setAttribute('viewBox', '0 0 16 16');
     searchIcon.setAttribute('fill', 'currentColor');
     searchIcon.classList.add('bi', 'bi-search', 'search-bar-search-icon');
-    searchIcon.setAttribute('viewBox', '0 0 16 16');
     searchIcon.innerHTML = `
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
     `;
@@ -45,7 +43,7 @@ function makeSearchBar(input) {
     clearBtn.type = 'button';
     clearBtn.classList.add('search-bar-clear-button', 'btn', 'rounded-circle', 'ripple', 'ripple-dark', 'ripple-centered', 'hover-glow');
     clearBtn.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
         </svg>
     `;
